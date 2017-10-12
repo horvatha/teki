dfbadd289202815b843caa2a1d786a83270182c4
Arpad Horváth <horvath.arpad.szfvar@gmail.com>
2017-10-12 12:47:55 +0200

    halado_peldak/mehsejt.py   teki névre neveztem a teknőst + szóközök

--- halado_peldak/mehsejt.py
+++ halado_peldak/mehsejt.py
@@ -1,42 +1,42 @@
 import turtle
 import random
 szin=random.choice(["red","purple","green","blue","brown"])
-toll=turtle.Turtle()
-toll.speed(0)
+teki=turtle.Turtle()
+teki.speed(0)
 
 def hatszog(oldalhossz):
-    toll.left(90)
+    teki.left(90)
     for i in range(6):
-        toll.forward(oldalhossz)
-        toll.right(60)
-    toll.right(120)
-    toll.forward(oldalhossz)
-    toll.left(60)
-    toll.forward(oldalhossz)
-    toll.right(30)
+        teki.forward(oldalhossz)
+        teki.right(60)
+    teki.right(120)
+    teki.forward(oldalhossz)
+    teki.left(60)
+    teki.forward(oldalhossz)
+    teki.right(30)
     
 
-def hatszogsor(oldalhossz,darab):
+def hatszogsor(oldalhossz, darab):
     for i in range(darab):
         hatszog(oldalhossz)
-    toll.right(150)
-    toll.up()
+    teki.right(150)
+    teki.up()
     for i in range(darab):
-        toll.forward(oldalhossz)
-        toll.right(60)
-        toll.forward(oldalhossz)
-        toll.left(60)
-    toll.left(120)
-    toll.forward(oldalhossz)
-    toll.right(60)
-    toll.forward(oldalhossz)
-    toll.left(90)
-    toll.down()
+        teki.forward(oldalhossz)
+        teki.right(60)
+        teki.forward(oldalhossz)
+        teki.left(60)
+    teki.left(120)
+    teki.forward(oldalhossz)
+    teki.right(60)
+    teki.forward(oldalhossz)
+    teki.left(90)
+    teki.down()
 
-def feladat(oldalhossz,darab,sor):
+def feladat(oldalhossz, darab, sor):
     for i in range(sor):
         szin=random.choice(["red","purple","green","blue","brown","orange"])
-        toll.color(szin)
+        teki.color(szin)
         hatszogsor(oldalhossz,darab)
 
 feladat(15,5,4)
