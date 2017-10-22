8037d114c04475f609e11b152f385d2759ff6232
Arpad Horvath <horvath.arpad.szfvar@gmail.com>
2017-10-22 10:24:02 +0200

    range_peldak.py bugfix

--- range_peldak.py
+++ range_peldak.py
@@ -1,4 +1,4 @@
-print(list(range(8)))        # [1, 2, 3, 4, 5, 6, 7]
+print(list(range(8)))        # [0, 1, 2, 3, 4, 5, 6, 7]
 print(list(range(1, 4)))     # [1, 2, 3]
 print(list(range(1, 9, 2)))  # [1, 3, 5, 7]
 
